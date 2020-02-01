cff5c17471ef8d7695a8a482b2eeb2364961e9d2
Ekaterinaryaguzova <56357023+Ekaterinaryaguzova@users.noreply.github.com>
2020-02-01 13:46:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const header = new Headers({
-	'Access-Control-Allow-Credentials': true, 
-  'Access-Control-Allow-Origin': '*'
-})
-
-const SE = new EventSource('http://localhost:8080/', header);
 const sub = document.querySelector('input#sub')
 
 function setCook(){
-	console.log("okk")
 	let city = document.querySelector('input#city')
 	console.log(city)
 }
