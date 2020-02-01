cbed6491133cfbde3dbfe82100219070720e43f1
Ekaterinaryaguzova <56357023+Ekaterinaryaguzova@users.noreply.github.com>
2020-02-01 14:10:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const sub = document.querySelector('input#sub')
+const sub = document.querySelector('.submit')
+const city = document.querySelector('.city')
 
-function setCook(){
-	let city = document.querySelector('input#city')
-	console.log(city)
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+  };
+
+  if (options.expires.toUTCString) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+  console.log(name=value)
 }
-sub.addEventListener("click", setCook)
+sub.addEventListener("onclick", setCook("city", city, {secure: true, 'max-age': 120}))
